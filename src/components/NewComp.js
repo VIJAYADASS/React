046769cf88359c36e09fca7a61bb49b076197cab
VIJAYADASS <Vijaydass0702@gmail.com>
2023-07-06 19:02:07 +0530

    if else using the button and icon

--- src/components/NewComp.js
+++ src/components/NewComp.js
@@ -7,33 +7,42 @@ const NewComp = () => {
     const [message, setMessage] = useState("Subscribe to react-Myapp");
     const [sub, setSub] = useState("Subscribe");
     const [imageURL, setImageURL] = useState(BellAImage);
-    
-    
+  
     const styles = {
       fontStyle: "bold",
       color: " rgb(22 101 52)",
     };
-  
-    const buttonChange = () => {
-      setMessage("Click the Bell icon and get the Notification");
-      setSub ("Subscribed") ;
-     
-    };
 
+    const buttonChange = () => {
+      if (sub === "Subscribe") {
+        setMessage("Click the Bell icon and get the Notification");
+        setSub("Subscribed");
+      } else {
+        setMessage("Subscribe to react-Myapp");
+        setSub("Subscribe");
+      }
+    };
+    
     const imageChange = () =>{
+      if (imageURL === BellAImage) {
         setImageURL(BellBImage);
         setMessage("Thank you! You will receive updates for all new videos and live streams.");
+      } else {
+        setImageURL(BellAImage);
+        setMessage("Click the Bell icon and get the Notification");
+      } 
     };
 
     return (
       <div className="App">
-        <h2 style={styles}>{message}</h2>
-       <b><button onClick={buttonChange}>{sub}</button></b> 
-        <p>
-        <img className="bell-icon" src={imageURL} onClick={imageChange}  alt="" />
-        </p>
-         
-      </div>
+      <h2 style={styles}>{message}</h2>
+      <b>
+        <button onClick={buttonChange}>{sub}</button>
+      </b>
+      <p>
+        <img className="bell-icon" src={imageURL} onClick={imageChange} alt="" />
+      </p>
+    </div>
     );
 };
 
